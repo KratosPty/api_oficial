432e85af6fe02580d74124fb847acec9efd7e96a
Stevens Cisneros <stevenscisneros507@gmail.com>
2023-06-28 02:21:09 +0000

    Update translation.py

--- translation.py
+++ translation.py
@@ -30,7 +30,7 @@ import csv
 from flask import Flask, request, jsonify
 
 from threading import Semaphore
-translation_engabe_blueprint = Blueprint('translation', __name__)
+translation= Blueprint('translation', __name__)
 
 
 semaphore = Semaphore(1)
