d2291bf44f5d21c27ceb87afb897a7c32b7f47a4
Steven Cisneros <107059883+KratosPty@users.noreply.github.com>
2023-06-29 16:42:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app.register_blueprint(translation_blueprint)
 app.register_blueprint(translation_ngabe_blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True)
